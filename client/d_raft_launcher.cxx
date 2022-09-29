491fbb2c92f0fedd0f09c1f17cb44e944c2276fb
Pronoy Roy <pronoyr@andrew.cmu.edu>
2022-09-29 05:46:33 -0700

    Fix compilation failure

--- client/d_raft_launcher.cxx
+++ client/d_raft_launcher.cxx
@@ -5,13 +5,15 @@
 #include <csignal>
 #include <sstream>
 #include <boost/program_options.hpp>
+#include "libnuraft/json.hpp"
 
 namespace po = boost::program_options;
+using json = nlohmann::json;
 
 void create_server(int id, std::string ip, int server_port, int client_port, std::string byz) {
     std::ostringstream sbuf;
     sbuf << "client/d_raft --id " << id << " --ip " << ip \
-    <<" --port " << server_port << " --cport " << client_port <<" --byz " << "false";
+    <<" --port " << server_port << " --cport " << client_port <<" --byz " << byz;
     int status = std::system(sbuf.str().c_str());
     if (status < 0)
         std::cout << "Error: " << strerror(errno) << '\n';
@@ -19,13 +21,13 @@ void create_server(int id, std::string ip, int server_port, int client_port, std
         if (WIFEXITED(status))
             std::cout << "Program returned normally, exit code " << WEXITSTATUS(status) << '\n';
         else
-            std::cout << "Program exited abnormaly\n";
+            std::cout << "Program exited abnormally\n";
     }
 }
 
-void create_client(int port, int num) {
+void create_client(int port, std::string path) {
     std::ostringstream cbuf;
-    cbuf << "client/d_raft_client --port " << port << " --num " << num;
+    cbuf << "client/d_raft_client --port " << port << " --path " << path;
     int status = std::system(cbuf.str().c_str());
     if (status < 0)
         std::cout << "Error: " << strerror(errno) << '\n';
@@ -33,7 +35,7 @@ void create_client(int port, int num) {
         if (WIFEXITED(status))
             std::cout << "Program returned normally, exit code " << WEXITSTATUS(status) << '\n';
         else
-            std::cout << "Program exited abnormaly\n";
+            std::cout << "Program exited abnormally\n";
     }
 }
 
@@ -47,68 +49,36 @@ int main(int argc, const char **argv)
 {
     std::signal(SIGINT,signal_handler);
 
-    int id, port, cport, num;
-    std::string ipaddr, byzantine;
-    po::options_description desc("Allowed options");
-    desc.add_options()("help",
-                       "produce help message")("id", po::value<int>(), "server id")(
-        "ip", po::value<std::string>(), "IP address")(
-        "port", po::value<int>(), "port number")(
-        "cport", po::value<int>(), "Client port number")(
-        "byz", po::value<std::string>(), "Byzantine status")(
-        "num", po::value<int>(), "Number of clients");
+    std::string config_file = "";
+    if( argc == 2 ) {
+        config_file = argv[1];
+    }
+    else {
+      std::cout << "Usage: ./d_raft_launcher config_file\n";
+      return 1;
+    }
+    std::ifstream f(config_file);
+    json data = json::parse(f);
 
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
+    int number_of_servers = data["server"].size();
+    int number_of_clients = data["client"].size();
 
-    if (vm.count("help")) {
-        std::cout << desc << "\n";
-        exit(0);
-    }
-      if (vm.count("id")) {
-        id = vm["id"].as<int>();
-    } else {
-        std::cout << "Server ID was not set.\n";
-        exit(1);
-    }
-    if (vm.count("port")) {
-        port = vm["port"].as<int>();
-    } else {
-        std::cout << "Port was not set.\n";
-        exit(1);
-    }
-    if (vm.count("cport")) {
-        cport = vm["cport"].as<int>();
-    } else {
-        std::cout << "Client port was not set.\n";
-        exit(1);
-    }
-    if (vm.count("ip")) {
-        ipaddr = vm["ip"].as<std::string>();
-    } else {
-        std::cout << "IP address not set.\n";
-        exit(1);
-    }
-    if (vm.count("byz")) {
-        byzantine = vm["byz"].as<std::string>();
-    } else {
-        std::cout << "Byzantine status not set.\n";
-        exit(1);
-    }
-    if (vm.count("num")) {
-        num = vm["num"].as<int>();
-    } else {
-        std::cout << "num of client not set.\n";
-        exit(1);
-    }
+    std::vector<std::thread> servers(number_of_servers);
+    std::vector<std::thread> clients(number_of_clients);
 
-    std::thread s(create_server,id, ipaddr, port, cport, byzantine);
+    for(int i = 0; i < number_of_servers; i++) {
+        servers.emplace_back(create_server, data["server"][i]["id"], data["server"][i]["ip"], data["server"][i]["port"], data["client"][i]["cport"],data["server"][i]["byzantine"]);
+    }
     
-    std::this_thread::sleep_for(std::chrono::seconds(5));
+    std::this_thread::sleep_for(std::chrono::seconds(10));
     
-    std::thread c(create_client, cport, num); 
+    for(int i = 0; i < number_of_clients; i++) {
+        std::cout << data["client"][i]["cport"] << " " << data["client"][i]["path"];
+        clients.emplace_back(create_client, data["client"][i]["cport"], data["client"][i]["path"]); 
+    }
 
-    c.join();
+    for(int i = 0; i < number_of_clients; i++) {
+        clients[i].join();
+    }
     return 0;
 }
