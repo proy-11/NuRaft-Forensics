e5d6dd0fe05d84cde28fc61b22cf2680cad3fd78
Pronoy Roy <pronoyr@andrew.cmu.edu>
2022-10-24 20:38:51 -0700

    Add cmd line args for workload values

--- client/d_raft_launcher.cxx
+++ client/d_raft_launcher.cxx
@@ -5,12 +5,14 @@
 #include "server_data_mgr.hxx"
 #include "utils.hxx"
 #include "workload.hxx"
+#include "libnuraft/json.hpp"
 #include <boost/program_options.hpp>
 #include <csignal>
 #include <cstdio>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 
 namespace po = boost::program_options;
 namespace fsys = std::filesystem;
@@ -120,18 +122,69 @@ int main(int argc, const char** argv) {
     std::signal(SIGPIPE, signal_handler);
 
     string config_file = "";
-    if (argc == 2) {
-        config_file = argv[1];
-    } else if (argc == 3) {
-        config_file = argv[1];
-        _PROG_LEVEL_ = std::atoi(argv[2]);
+    int size = 0, batch_size = 0;
+    float frequency = 0.0;
+
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("config_file", po::value<std::string>()->required(), "config file path")
+        ("size", po::value<int>()->default_value(0), "total size")
+        ("freq", po::value<float>()->default_value(0.0), "frequency")
+        ("batch_size", po::value<int>()->default_value(0), "batch size")
+        ("log_level", po::value<int>()->default_value(2), "print log level")
+    ;
+    
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+    
+
+    if (vm.count("help")) {
+        std::cout << _C_BOLDYELLOW_ << desc << "\n";
+        exit(0);
+    }
+
+    if(vm.count("config_file")) {
+        config_file = vm["config_file"].as<std::string>();
     } else {
-        std::cout << _C_BOLDYELLOW_ << "Usage: ./d_raft_launcher config_file <PRINT_LEVEL>\n";
-        return 1;
+        std::cout << "config file not set.\n";
+        exit(1);
+    }
+
+    if(vm.count("size")) {
+        size = vm["size"].as<int>();
+    }
+
+    if(vm.count("freq")) {
+        frequency = vm["freq"].as<float>();
+    }
+
+    if(vm.count("batch_size")) {
+        batch_size = vm["batch_size"].as<int>();
+    }
+
+    if(vm.count("log_level")) {
+        _PROG_LEVEL_ = vm["log_level"].as<int>();
     }
 
     std::ifstream f(config_file);
     meta_setting = json::parse(f);
+
+    if(size != 0 && frequency != 0.0 && batch_size != 0) {
+        using json = nlohmann::basic_json<std::map, std::vector, std::string, bool, std::int64_t, std::uint64_t, float>;
+        
+        json workload_json;
+        workload_json["size"] = size;
+        std::stringstream ss;
+        ss << std::setprecision(4) << frequency;
+        workload_json["freq"] = std::stof(ss.str());
+        workload_json["batch_size"] = batch_size;
+        workload_json["type"] = "UNIF";
+
+        std::ofstream file(meta_setting["client"]["path"]);
+        file << std::setw(4) << workload_json << std::endl;
+    }
 
     server_mgr = std::shared_ptr<server_data_mgr>(new server_data_mgr(meta_setting["server"]));
 
