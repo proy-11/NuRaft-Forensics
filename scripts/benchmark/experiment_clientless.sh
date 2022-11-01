593e22478ae09a412a2bc9390197f5d8df1819bc
WeizhaoT <twz17519@gmail.com>
2022-10-31 22:40:28 -0400

    scripts

--- scripts/benchmark/experiment_clientless.sh
+++ scripts/benchmark/experiment_clientless.sh
@@ -2,8 +2,8 @@
 scp ../client/*.json client/
 rm -rf ./**.log
 
-for bs in 100000; do
-    for i in {2..2}; do
+for bs in 100000 10000 1000 100 10; do
+    for i in {1..5}; do
         bash ./check-port.sh 23333 23338
         ./client/d_raft_launcher_clientless \
             --config-file ./client/config.json \
@@ -16,13 +16,3 @@ for bs in 100000; do
         sleep 1
     done
 done
-
-# gdb --args ./client/d_raft_launcher_clientless \
-#     --config-file ./client/config.json \
-#     --log-level 1 \
-#     --batch-size 100 \
-#     --freq 5.0 \
-#     --size 3000 \
-#     --datadir './data/debug'
-
-# ./client/d_raft_launcher_clientless --config-file ./client/config.json --log-level 1 --batch-size 1000 --freq 10.0 --size 600000 --datadir './data/debug'
