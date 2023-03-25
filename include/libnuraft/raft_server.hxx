645c26a2dd082bec2872bc44188277bd10ad6e4e
Pronoy Roy <pronoyr@andrew.cmu.edu>
2023-03-25 10:24:18 -0700

    Update key information

--- include/libnuraft/raft_server.hxx
+++ include/libnuraft/raft_server.hxx
@@ -828,12 +828,12 @@ protected:
     /**
      * FMARK: Private key
      */
-    ptr<seckey_intf> private_key;
+    ptr<seckey_intf> private_key_;
 
     /**
      * FMARK: Public key
      */
-    ptr<pubkey_intf> public_key;
+    ptr<pubkey_intf> public_key_;
 
     /**
      * @brief FMARK: finished cc
